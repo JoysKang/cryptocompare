b2c0e103bf08125eb665100e33e17837c21ee5fb
Xiaokang Fan <xiaokang.fan@eigen.capital>
2023-08-09 15:36:08 +0800

    search

---
+++ src/api.tsx
@@ -0,0 +1,79 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+
+const cryptos = "BTC,ETH,USDT";
+const cryptoList: string[] = ["BTC", "ETH", "USDT"];
+const fiats = "USD,EUR,AUD,CNY";
+const fiatSymbol = {
+  USD: "$",
+  EUR: "€",
+  AUD: "A$",
+  CNY: "¥",
+};
+
+const instance = axios.create({
+  baseURL: "https://min-api.cryptocompare.com/data/pricemulti?tsyms=" + fiats + "&fsyms=" + cryptos,
+  headers: { "Content-type": "application/json" },
+});
+
+const responseBody = (response: AxiosResponse) => response.data;
+
+const requests = {
+  get: (url: string, config: AxiosRequestConfig = {}) => instance.get(url, config).then(responseBody),
+};
+
+export async function fetchData(searchCryptos: string) {
+  try {
+    let response: AxiosResponse;
+    if (!searchCryptos || searchCryptos == undefined) {
+      response = await requests.get(
+        "https://min-api.cryptocompare.com/data/pricemultifull?tsyms=" + fiats + "&fsyms=" + cryptos
+      );
+    } else {
+      searchCryptos = searchCryptos.toUpperCase();
+      console.log(
+        "88888",
+        searchCryptos,
+        "https://min-api.cryptocompare.com/data/pricemultifull?tsyms=" +
+          fiats +
+          "&fsyms=" +
+          cryptos +
+          "," +
+          searchCryptos
+      );
+      cryptoList.push(searchCryptos);
+      console.log(cryptoList);
+      response = await requests.get(
+        "https://min-api.cryptocompare.com/data/pricemultifull?tsyms=" +
+          fiats +
+          "&fsyms=" +
+          cryptos +
+          "," +
+          searchCryptos
+      );
+    }
+
+    // console.log(JSON.stringify(response));
+    if (!response) {
+      throw new Error("Failed to fetch the coin list");
+    }
+
+    const newPriceData = cryptoList.map((item) => {
+      let priceStr = "";
+      let icon = "";
+      const crypto = response.RAW[item];
+      for (const [key, value] of Object.entries(fiatSymbol)) {
+        if (crypto[key]) {
+          priceStr += value + crypto[key].PRICE + "  ";
+          icon = "https://cryptocompare.com" + crypto[key].IMAGEURL;
+        }
+      }
+
+      return { icon: icon, name: item, price: priceStr };
+    });
+
+    return newPriceData;
+  } catch (e) {
+    console.log(e);
+    throw new Error("Failed to fetch the coin list");
+  }
+}
--- src/price.tsx
+++ src/price.tsx
@@ -1,17 +1,6 @@
-import { List, showToast, Toast, Action, ActionPanel } from "@raycast/api";
+import { List, Action, ActionPanel } from "@raycast/api";
 import { useEffect, useState } from "react";
-import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
-
-// 默认的数字货币
-const cryptos = "BTC,ETH,USDT";
-const cryptoList: string[] = ["BTC", "ETH", "USDT"];
-const fiats = "USD,EUR,AUD,CNY";
-const fiatSymbol = {
-  USD: "$",
-  EUR: "€",
-  AUD: "A$",
-  CNY: "¥",
-};
+import { fetchData } from "./api";
 
 interface Data {
   icon: string;
@@ -19,56 +8,32 @@ interface Data {
   price: string;
 }
 
-const instance = axios.create({
-  baseURL: "https://min-api.cryptocompare.com/data/pricemulti?tsyms=" + fiats + "&fsyms=" + cryptos,
-  headers: { "Content-type": "application/json" },
-});
-
-const responseBody = (response: AxiosResponse) => response.data;
-
-const requests = {
-  get: (url: string, config: AxiosRequestConfig = {}) => instance.get(url, config).then(responseBody),
-};
-
 export default function Command() {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [priceData, setPriceData] = useState<Data[]>([]);
 
   useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await requests.get(
-          "https://min-api.cryptocompare.com/data/pricemultifull?tsyms=" + fiats + "&fsyms=" + cryptos
-        );
-        const newPriceData = cryptoList.map((item) => {
-          let priceStr = "";
-          let icon = "";
-          const crypto = response.RAW[item];
-          for (const [key, value] of Object.entries(fiatSymbol)) {
-            if (crypto[key]) {
-              priceStr += value + crypto[key].PRICE + "  ";
-              icon = "https://cryptocompare.com" + crypto[key].IMAGEURL;
-            }
-          }
-
-          return { icon: icon, name: item, price: priceStr };
-        });
-        setPriceData(newPriceData);
-      } catch (e) {
-        await showToast({
-          style: Toast.Style.Failure,
-          title: "Failed to fetch the coin list" + e,
-        });
-      }
-
+    async function fetchDataAsync() {
+      const newPriceData = await fetchData("");
+      setPriceData(newPriceData);
       setIsLoading(false);
     }
 
-    fetchData();
+    fetchDataAsync();
   }, []);
 
   return (
-    <List isLoading={isLoading}>
+    <List
+      isLoading={isLoading}
+      filtering={false}
+      onSearchTextChange={async (SearchText) => {
+        setIsLoading(true);
+        setPriceData(await fetchData(SearchText));
+        setIsLoading(false);
+      }}
+      throttle={true}
+      searchBarPlaceholder="Search for prices of digital coins"
+    >
       {priceData.map((item) => (
         <List.Item
           key={item.name}
